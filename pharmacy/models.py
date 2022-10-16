067940d980129bce37dd9ea26036b3f565f18aa8
naufalweise <naufalwwidyatama@gmail.com>
2022-10-16 08:51:12 +0700

    Add pharmacy & medicine model

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pharmacy(models.Model):
+	name = models.CharField(max_length=100)
+
+
+class Medicine(models.Model):
+	name = models.CharField(max_length=100)
+	stock = models.IntegerField()
+	pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+
+
+	
