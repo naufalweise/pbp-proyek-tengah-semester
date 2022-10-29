02966a66c1a2d076e2505fbbd8c7765b2c689cea
naufalweise <naufalwwidyatama@gmail.com>
2022-10-29 18:13:34 +0700

    Add authorizations in medicine views

--- medicine/views.py
+++ medicine/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import permission_required
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -6,6 +7,7 @@ from .forms import MedicineForm
 from .models import Medicine
 
 # Create your views here.
+@permission_required('medicine.add_medicine')
 @require_POST
 def create_medicine(request):
 	form = MedicineForm(request.POST)
@@ -18,27 +20,31 @@ def retrieve_medicines(request):
 	medicines = Medicine.objects.all()
 	return HttpResponse(serializers.serialize("json", medicines), content_type="application/json")
 
+@permission_required('medicine.change_medicine')
 def update_medicine(request, id):
 	instance = Medicine.objects.get(pk = id)
 	form = MedicineForm(request.POST, instance=instance)
 	form.save()
 	return HttpResponse(status=200)
 
+@permission_required('medicine.delete_medicine')
 @require_POST
 def delete_medicine(request, id):
 	instance = Medicine.objects.get(pk = id)
 	instance.delete()
 	return HttpResponse(status=200)
 
-
+@permission_required('medicine.change_medicine')
 def view_crud_page (request):
 	form = MedicineForm()
 	return render(request, "medicine/crud.html", {'form': form})
 
+@permission_required('medicine.add_medicine')
 def get_crud_form_empty(request):
 	form = MedicineForm()
 	return JsonResponse({'form': form.as_div()})
 
+@permission_required('medicine.change_medicine')
 def get_crud_form(request, id):
 	instance = Medicine.objects.get(pk = id)
 	form = MedicineForm(instance=instance)
