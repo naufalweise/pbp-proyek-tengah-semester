cd13cb3be0458e82a6f728c0fc881c14e85f5377
nadhirashahnaz25 <nadhirashahnaz25@gmail.com>
2022-11-02 09:12:26 +0700

    login

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -7,7 +7,10 @@ from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from .models import Pharmacy
 from .forms import PharmacyForm
+from django.contrib.auth.decorators import permission_required
+from datetime import datetime
 
+@permission_required('pharmacy.add_pharmacy')
 @require_POST
 def create_pharmacy(request):
     form = PharmacyForm(request.POST)
@@ -21,26 +24,31 @@ def retrieve_pharmacy(request):
     pharmacy = Pharmacy.objects.all()
     return HttpResponse(serializers.serialize("json",pharmacy), content_type="application/json")
 
+@permission_required('pharmacy.change_pharmacy')
 def update_pharmacy(request, id):
 	pharm = Pharmacy.objects.get(pk = id)
 	form = PharmacyForm(request.POST, instance=pharm)
 	form.save()
 	return HttpResponse(status=200)
 
+@permission_required('pharmacy.delete_pharmacy')
 @require_POST
 def delete_pharmacy(request, id):
 	pharm = Pharmacy.objects.get(pk = id)
 	pharm.delete()
 	return HttpResponse(status=200)
 
+@permission_required('pharmacy.change_pharmacy')
 def view_crud_page (request):
 	form = PharmacyForm()
 	return render(request, "pharmacy/crud.html", {'form': form})
 
+@permission_required('pharmacy.add_pharmacy')
 def get_crud_form_empty(request):
 	form = PharmacyForm()
 	return JsonResponse({'form': form.as_div()})
 
+@permission_required('pharmacy.change_pharmacy')
 def get_crud_form(request, id):
 	pharm = Pharmacy.objects.get(pk = id)
 	form = PharmacyForm(instance=pharm)
